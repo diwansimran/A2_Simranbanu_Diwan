bf0c07fa7dfdc7efc1ddfb08e946fcc29db6cc99
diwansimran <diwansimran98@gmail.com>
2020-06-07 19:54:02 -0300

    initial commit

---
+++ findadobe/build/precache-manifest.5523650230e009ccfdc672d68d437d55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e1f0b7515221a7b14a13efbb134c49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9c0f0eeec670267c888",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdd92cae98d44b0dde8f",
+    "url": "/static/js/2.6cd49955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cd49955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c0f0eeec670267c888",
+    "url": "/static/js/main.b1b99618.chunk.js"
+  },
+  {
+    "revision": "881e350b6f5a5708dfb5",
+    "url": "/static/js/runtime-main.d3365d80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(__dirname + '/findadobe/build/'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/findadobe/build/index.html'));
+})
+console.log(__dirname)
+//server port
+app.listen(process.env.PORT || 3000);
